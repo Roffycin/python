0aaae7ffade95034ef8d892d0a8ccf9c4c90c71a
Roicin c <nicolasgutierrez295@gmail.com>
2025-06-01 16:26:43 -0400

    pedir valores

--- funciones.py
+++ funciones.py
@@ -10,6 +10,7 @@ def multiplicar(num1,num2):
 def dividir(num1,num2):
     return num1 / num2
 
-def valores(num1,num2):
+def valores():
     num1 = int(input("Ingrese el primer valor: "))
     num2 = int(input("Ingrese el segundo valor: "))
+    return num1,num2
