fa6389238b21a7e678743b91586bd9932d6baab2
Roicin c <nicolasgutierrez295@gmail.com>
2025-06-01 16:31:53 -0400

    pedir valores con tope

--- funciones.py
+++ funciones.py
@@ -11,6 +11,6 @@ def dividir(num1,num2):
     return num1 / num2
 
 def valores():
-    num1 = int(input("Ingrese el primer valor: "))
-    num2 = int(input("Ingrese el segundo valor: "))
-    return num1,num2
+    while num1 != 0:
+        num1 = int(input("Ingrese los valores (0 para salir): "))
+        return num1
